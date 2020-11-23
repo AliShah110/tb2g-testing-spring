73245fc905733a451d521aeb5eb4dbc761ad0f8c
Kazmi <kazmi@itemis.de>
2020-11-23 20:35:58 +0100

    Inner Configuration Class with @ComponentScan. Search for Components in the give package. WordProducer marked as "Primary" is taken.

--- src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
+++ src/test/java/org/springframework/samples/petclinic/sfg/junit5/HearingInterpreterComponentScanTest.java
@@ -2,25 +2,28 @@ package org.springframework.samples.petclinic.sfg.junit5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.samples.petclinic.sfg.BaseConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.samples.petclinic.sfg.HearingInterpreter;
-import org.springframework.samples.petclinic.sfg.LaurelConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringJUnitConfig(classes = {BaseConfig.class, LaurelConfiguration.class})
-class HearingInterpreterTest {
+@SpringJUnitConfig
+class HearingInterpreterComponentScanTest {
+
+    @Configuration
+    @ComponentScan("org.springframework.samples.petclinic.sfg")
+    static class TestConfig{
+
+    }
 
     @Autowired
     HearingInterpreter hearingInterpreter;
 
     @Test
     void whatIheard() {
-        assertEquals("Laurel", hearingInterpreter.whatIheard());
-    }
-
-    @Test
-    void testWhatIheard() {
+        String word = hearingInterpreter.whatIheard();
+        assertEquals("Laurel", word);
     }
 }
