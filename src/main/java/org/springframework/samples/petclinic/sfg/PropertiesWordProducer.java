b3d32fa529c57652b24285f41cb4f8aee1c7806e
Kazmi <kazmi@itemis.de>
2020-11-30 14:11:45 +0100

    Assignment - Laura Properties

--- src/main/java/org/springframework/samples/petclinic/sfg/PropertiesWordProducer.java
+++ src/main/java/org/springframework/samples/petclinic/sfg/PropertiesWordProducer.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Primary
-@Profile("externalized")
+@Profile({"externalized", "laurel-properties"})
 @Component
 public class PropertiesWordProducer implements WordProducer{
 
